43dc5d31460686ac80c586fdcdcbf4f12c9ad17c
rizan1998 <rizanalfalah@gmail.com>
2023-11-16 21:13:38 +0700

    install importannt package setup server config in package.json first running server

--- app/server.js
+++ app/server.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("server is running...");
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
