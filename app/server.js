46b383dd71e890fbb93d21a6823eb7a7a733c761
rizan1998 <rizanalfalah@gmail.com>
2023-11-17 07:01:46 +0700

    (unfinished) create register user api

--- app/server.js
+++ app/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
-//
+
+//Routes
+import userRoutes from "./routes/userRoutes";
 
 dotenv.config();
 connectDB();
@@ -11,5 +13,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("server is running...");
 });
+
+app.use("/api/users", userRoutes);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
