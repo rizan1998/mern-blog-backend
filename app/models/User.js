08cb5b22c604e79e5cfe847c1a5c2dcb594e9f4e
rizan1998 <rizanalfalah@gmail.com>
2023-11-21 05:58:35 +0700

    create login function

--- app/models/User.js
+++ app/models/User.js
@@ -1,5 +1,5 @@
 import { Schema, model } from "mongoose";
-import { hash } from "bcryptjs";
+import { hash, compare } from "bcryptjs";
 import { sign } from "jsonwebtoken";
 
 const UserSchema = new Schema(
@@ -25,6 +25,10 @@ UserSchema.pre("save", async function (next) {
 
 UserSchema.methods.generateJWT = async function () {
   return await sign({ id: this._id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+};
+
+UserSchema.methods.comparePassword = async function (enteredPassword) {
+  return await compare(enteredPassword, this.password);
 };
 
 const User = model("User", UserSchema);
