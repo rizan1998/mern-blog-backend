171920c431f1997a0a64f343936f2220e1ca0d2b
rizan1998 <rizanalfalah@gmail.com>
2023-11-21 22:17:09 +0700

    unifinished update image

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,6 +1,7 @@
+import { uploadPicture } from "../middleware/uploadPirctureMiddleware";
 import User from "../models/User";
 
-export const registerUser = async (req, res, next) => {
+const registerUser = async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
 
@@ -31,7 +32,7 @@ export const registerUser = async (req, res, next) => {
   }
 };
 
-export const loginUser = async (req, res, next) => {
+const loginUser = async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
@@ -59,7 +60,7 @@ export const loginUser = async (req, res, next) => {
   }
 };
 
-export const userProfile = async (req, res, next) => {
+const userProfile = async (req, res, next) => {
   try {
     let user = await User.findById(req.user._id);
     if (user) {
@@ -81,4 +82,46 @@ export const userProfile = async (req, res, next) => {
   }
 };
 
-export { registerUser, loginUser };
+const updateProfile = async (req, res, next) => {
+  try {
+    let user = await User.findById(req.user._id);
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password && req.body.password.length < 6) {
+      throw new Error("Password length must be at least 6 character");
+    } else if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUserProfile = await user.save();
+
+    res.json({
+      _id: updatedUserProfile._id,
+      avatar: updatedUserProfile.avatar,
+      name: updatedUserProfile.name,
+      email: updatedUserProfile.email,
+      verified: updatedUserProfile.verified,
+      admin: updatedUserProfile.admin,
+      token: await updatedUserProfile.generateJWT(),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const udpateProfilePicture = async (req, res, next) => {
+  try {
+    const upload = uploadPicture.single("profilePicture");
+    upload(req, res, async function (err) {
+      if (err) {
+        const error = new Error("An known error occured when uploading ")
+      }
+    };)
+  } catch (error) {}
+};
+
+export { registerUser, loginUser, userProfile, updateProfile, udpateProfilePicture };
--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { loginUser, registerUser, userProfile } from "../controllers/userController";
+import { loginUser, registerUser, udpateProfilePicture, updateProfile, userProfile } from "../controllers/userController";
 import { authGuard } from "../middleware/authMIddleware";
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/profile", authGuard, userProfile);
+router.put("/updateProfile", authGuard, updateProfile);
+router.put("/updateProfilePicture", authGuard, udpateProfilePicture);
 
 export default router;
