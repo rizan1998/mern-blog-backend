da5b1ac1d608cdc9ca645e3778e7cd5fad94882d
rizan1998 <rizanalfalah@gmail.com>
2024-02-10 17:57:37 +0700

    add cors extention and change server.js

--- app/server.js
+++ app/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import connectDB from "./config/db";
+import cors from "cors";
 import { errorResponserHandler, invalidPathHandler } from "./middleware/errorHandler";
 
 //Routes
@@ -10,12 +12,16 @@ dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("server is running...");
 });
 
 app.use("/api/users", userRoutes);
+
+// static assets
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(invalidPathHandler);
 app.use(errorResponserHandler);
