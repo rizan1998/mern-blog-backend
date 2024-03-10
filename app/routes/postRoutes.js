59a78b120c5739531d7cf8e2e8600ba16bb55008
rizan1998 <rizanalfalah@gmail.com>
2024-03-10 09:43:09 +0700

    create api for get all data posts

--- app/routes/postRoutes.js
+++ app/routes/postRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
 const router = express.Router();
-import { createPost, updatePost, deletePost, getPost } from "../controllers/postController";
+import { createPost, updatePost, deletePost, getPost, getAllPosts } from "../controllers/postController";
 import { adminGuard, authGuard } from "../middleware/authMIddleware";
 
-router.post("/", authGuard, adminGuard, createPost);
+router.route("/").post(authGuard, adminGuard, createPost).get(getAllPosts);
 router.route("/:slug").put(authGuard, adminGuard, updatePost).delete(authGuard, adminGuard, deletePost).get(getPost);
 
 export default router;
